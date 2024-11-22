02a4bb95b31d650b58066e70dd92f3f80bc616ff
Andreea Suciu <andreea.suciu@emanuel.ro>
2024-11-22 11:06:00 +0200

    final

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ export default defineConfig({
    base: '',
    build:{
     rollupOptions: {
-      input: [ "./index.html"]
+      input: [ "./index.html", "./about.html", "./book-appointment.html", "./bridal.html", "./coloring.html" , "./contact.html" , "./haircuts.html", "./services.html", "./stylists.html", "./texturizing.html"]
     }
   }
 })
